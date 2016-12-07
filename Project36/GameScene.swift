d610a7a48c46a7b5c30320a66358077e7445f8d7
JP Deguzman <jddeguzman@ucdavis.edu>
2016-12-07 13:14:58 -0800

    First commit.

---
+++ Project36/GameScene.swift
@@ -0,0 +1,39 @@
+//
+//  GameScene.swift
+//  Project36
+//
+//  Created by Jonathan Deguzman on 12/6/16.
+//  Copyright © 2016 Jonathan Deguzman. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var player: SKSpriteNode!
+    
+    override func didMove(to view: SKView) {
+        createPlayer()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+    
+    func createPlayer() {
+        let playerTexture = SKTexture(imageNamed: "player-1")
+        player = SKSpriteNode(texture: playerTexture)
+        player.zPosition = 10
+        player.position = CGPoint(x: frame.width / 10, y: frame.height * 0.75)
+        
+        addChild(player)
+        
+        let frame2 = SKTexture(imageNamed: "player-2")
+        let frame3 = SKTexture(imageNamed: "player-3")
+        let animation = SKAction.animate(with: [playerTexture, frame2, frame3, frame2], timePerFrame: 0.01)
+        let runForever = SKAction.repeatForever(animation)
+        
+        player.run(runForever)
+    }
+}
