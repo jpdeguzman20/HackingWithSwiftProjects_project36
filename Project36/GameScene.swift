a9547f112b91953cb3a7fd8f5e82153a622c4362
JP Deguzman <jddeguzman@ucdavis.edu>
2016-12-07 13:54:03 -0800

    Added player to scene.

--- Project36/GameScene.swift
+++ Project36/GameScene.swift
@@ -21,16 +21,22 @@ class GameScene: SKScene {
 
     }
     
+    /// createPlayer() creates the player (a.k.a. the plane) animation
+    /// - Returns: nil
+    /// - Parameters: none
+    
     func createPlayer() {
         let playerTexture = SKTexture(imageNamed: "player-1")
         player = SKSpriteNode(texture: playerTexture)
         player.zPosition = 10
+        // Position the player most of the way to the top of the screen and most of the way to the left.
         player.position = CGPoint(x: frame.width / 10, y: frame.height * 0.75)
         
         addChild(player)
         
         let frame2 = SKTexture(imageNamed: "player-2")
         let frame3 = SKTexture(imageNamed: "player-3")
+        // To get the propeller animation, pass in an array of textures to the animate(with:) method and cycle through each from as fast as possible.
         let animation = SKAction.animate(with: [playerTexture, frame2, frame3, frame2], timePerFrame: 0.01)
         let runForever = SKAction.repeatForever(animation)
         
