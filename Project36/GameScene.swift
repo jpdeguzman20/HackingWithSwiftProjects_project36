bb70b0b96bb494bcb4dc3c491aadd4e891f8ef73
JP Deguzman <jddeguzman@ucdavis.edu>
2016-12-07 16:53:00 -0800

    Added scrolling background and ground.

--- Project36/GameScene.swift
+++ Project36/GameScene.swift
@@ -15,6 +15,9 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         createPlayer()
+        createSky()
+        createBackground()
+        createGround()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -41,5 +44,78 @@ class GameScene: SKScene {
         let runForever = SKAction.repeatForever(animation)
         
         player.run(runForever)
+    }
+    
+    /// createSky() creates the sky background of the screen
+    /// - Returns: nil
+    /// - Parameters: none
+    
+    func createSky() {
+        // Create the top part of the sky that will take up 67% of the screen and use the anchorPoint property to make it measure up from the center top of the screen
+        let topSky = SKSpriteNode(color: UIColor(hue: 0.55, saturation: 0.14, brightness: 0.97, alpha: 1), size: CGSize(width: frame.width, height: frame.height * 0.67))
+        topSky.anchorPoint = CGPoint(x: 0.5, y: 1)
+        
+        // Create the bottom part of the sky that will take up 33% of the screen and use anchorPoint to do the same thing as mentioned above.
+        let bottomSky = SKSpriteNode(color: UIColor(hue: 0.55, saturation: 0.16, brightness: 0.96, alpha: 1), size: CGSize(width: frame.width, height: frame.height * 0.33))
+        topSky.anchorPoint = CGPoint(x: 0.5, y: 1)
+        
+        topSky.position = CGPoint(x: frame.midX, y: frame.height)
+        bottomSky.position = CGPoint(x: frame.midX, y: bottomSky.frame.height / 2)
+        
+        addChild(topSky)
+        addChild(bottomSky)
+        
+        bottomSky.zPosition = -40
+        topSky.zPosition = -40
+    }
+    
+    /// createBackground() creates the scrolling mountain background on the screen
+    /// - Returns: nil
+    /// - Parameters: none
+    
+    func createBackground() {
+        let backgroundTexture = SKTexture(imageNamed: "background")
+        
+        for i in 0 ... 1 {
+            let background = SKSpriteNode(texture: backgroundTexture)
+            background.zPosition = -30
+            background.anchorPoint = CGPoint.zero
+            
+            // Calculate the x position of each mountain. Since the loop goes from 0 to 1, the first time the loop executes, X will be zero. The second time it executes, X will be the width of the texture minus 1. This prevents any gaps.
+            background.position = CGPoint(x: (backgroundTexture.size().width * CGFloat(i)) - CGFloat(1 - i), y: 100)
+            
+            addChild(background)
+            
+            // Both mountains will move left a distance equal to its width, jump back another distance the size of its width, and repeat the sequence forever. Combined, these steps create an infinite scrolling landscape.
+            let moveLeft = SKAction.moveBy(x: -backgroundTexture.size().width, y: 0, duration: 20)
+            let moveReset = SKAction.moveBy(x: backgroundTexture.size().width, y: 0, duration: 0)
+            let moveLoop = SKAction.sequence([moveLeft, moveReset])
+            let moveForever = SKAction.repeatForever(moveLoop)
+            
+            background.run(moveForever)
+        }
+    }
+    
+    /// createGround() creates the scrolling ground background on the screen
+    /// - Returns: nil
+    /// - Parameters: none
+    
+    func createGround() {
+        let groundTexture = SKTexture(imageNamed: "ground")
+        
+        for i in 0 ... 1 {
+            let ground = SKSpriteNode(texture: groundTexture)
+            ground.zPosition = -10
+            ground.position = CGPoint(x: (groundTexture.size().width / 2.0 + (groundTexture.size().width * CGFloat(i))), y: groundTexture.size().height / 2.0)
+            
+            addChild(ground)
+            
+            let moveLeft = SKAction.moveBy(x: -groundTexture.size().width, y: 0, duration: 5)
+            let moveReset = SKAction.moveBy(x: groundTexture.size().width, y: 0, duration: 0)
+            let moveLoop = SKAction.sequence([moveLeft, moveReset])
+            let moveForever = SKAction.repeatForever(moveLoop)
+            
+            ground.run(moveForever)
+        }
     }
 }
